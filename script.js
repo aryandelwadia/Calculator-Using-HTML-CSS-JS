9c4959b68237614bf9a3ab3045a6040b9c4b4cf7
Aryan Delawadia <127605017+aryandelwadia@users.noreply.github.com>
2023-08-17 21:48:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,67 @@
-let string = "";
-let btns = document.querySelectorAll(".buttons");
+<!DOCTYPE html>
+<html lang="en">
 
-Array.from(btns).forEach((button) => {
-    button.addEventListener('click', (e) => {
-        if (e.target.innerHTML == "=") {
-            string = eval(string);
-            document.querySelector('input').value = string;
-        }
-        else if (e.target.innerHTML == "C") {
-            string = "";
-            document.querySelector('input').value = string;
-        }
-        else {
-            console.log(e.target)
-            string = string + e.target.innerHTML;
-            document.querySelector('input').value = string;
-        }
-    })
-})
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculator</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <div class="bg">
+        <header>
+            <div class="navbar">Calculator</div>
+        </header>
+        
+        <div class="calculator">
+            <input type="text" class="txtarea">
+            <div class="buttonlayout">
+                <table>
+                    <tr>
+                    <td><button class="buttons" id="7">7</button></td>
+                    <td><button class="buttons" id="8">8</button></td>
+                    <td><button class="buttons" id="9">9</button></td>
+                    <td><button class="buttons" id="C">C</button></td>
+                    </tr>
+                    <tr>
+                    <td><button class="buttons" id="4">4</button></td>
+                    <td><button class="buttons" id="5">5</button></td>
+                    <td><button class="buttons" id="6">6</button></td>
+                    <td><button class="buttons" id="DL"><i class="fa-solid fa-delete-left"></i></button></td>
+                </tr>
+                <tr>
+                    <td><button class="buttons" id="1">1</button></td>
+                    <td><button class="buttons" id="2">2</button></td>
+                    <td><button class="buttons" id="3">3</button></td>
+                    <td><button class="buttons" id="*">*</button></td>
+                </tr>
+                <tr>
+                    <td><button class="buttons" id="0">0</button></td>
+                    <td><button class="buttons" id="00">00</button></td>
+                    <td><button class="buttons" id=".">.</button></td>
+                    <td><button class="buttons" id="/">/</button></td>
+                </tr>
+                <tr>
+                    <td><button class="buttons" id="%">%</button></td>
+                    <td><button class="buttons" id="=">=</button></td>
+                    <td><button class="buttons" id="-">-</button></td>
+                    <td><button class="buttons" id="+">+</button></td>
+                </tr>
+                </table>
+            </div>
+        </div>
+        <footer>
+            <div class="navbar footbar">
+                This Calculator is made for basic arithmetic operations using  HTML CSS JS.
+            </div>
+        </footer>
+    </div>
+    <script src="script.js"></script>
+</body>
+
+</html>
